5c9f21e40c02bddf40e2cdf255862328f098309a
HarshithaPerumalla <harshu0989@gmail.com>
2024-08-23 18:09:31 +0530

    updated

---
+++ envloader.py
@@ -0,0 +1,6 @@
+from dotenv import dotenv_values
+env_tokens=dotenv_values("../.env")
+
+TRAINING_DATA=env_tokens['TRAINING_DATA']
+VALIDATION_DATA=env_tokens['VALIDATION_DATA']
+PREPROCESSED_DATA=env_tokens['PREPROCESSED_DATA']
